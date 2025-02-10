86ad160709c3ef18284eff07b7019569dedf3322
{mayur777-ui} <{mayurlaxkar76@gamil.com}>
2025-02-10 18:00:12 +0530

    Added login and register pages with layout

---
+++ first-nextapp/src/app/(auth)/(auth-with-layouts)/layout.tsx
@@ -0,0 +1,15 @@
+export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+        <div>
+            <h2>
+                Inner layout
+            </h2>
+            {children}
+        </div>
+    )
+  }
+  
